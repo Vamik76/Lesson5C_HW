a1bb17fbc924adc49b042c46530dda482332a983
Vamik76 <merts_v@mail.ru>
2022-12-09 19:05:21 +1000

    HW5C#_task2end

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+Random random = new Random();
+int[]arr=new int [7];
+for (int i=0; i<arr.Length;i++)
+    arr[i]=random.Next(10, 100);
+Console.WriteLine(string.Join(", ",arr));
+Console.WriteLine("Sum of Elemets on odd index: "+OddSumElements(arr));
+
+int OddSumElements(int[] array)
+{
+    int i=0,b=0,sum=0;
+    for ( i=0;i<array.Length;i++)
+        if (b==i%2)
+            sum=sum+array[i];
+    return sum;
+}
