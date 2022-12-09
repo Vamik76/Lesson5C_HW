107b115d3761528c4b9393daf24b93d837fa38a0
Vamik76 <merts_v@mail.ru>
2022-12-10 01:53:30 +1000

    HW5C#_task3+realnumbers

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,15 +2,16 @@
 //[3 7 22 2 78] -> 76
 //Диапазон любой, ввод зависит от пользователя. (размер, значения)
 Random random = new Random();
-int[]arr=new int [7];
+double[]arr=new double [7];
 for (int i=0; i<arr.Length;i++)
     arr[i]=random.Next(0, 100);
 Console.WriteLine(string.Join(", ",arr));
 Console.WriteLine("Difference between Max and Min Elements: "+DiffMaxMin(arr));
 
-int DiffMaxMin(int[] array)
+double DiffMaxMin(double[] array)
 {
-    int i=1,diff=0,max=array[0],min=array[0];
+    int i=1;
+    double diff=0,max=array[0],min=array[0];
     for (i=1;i<array.Length;i++)
     {
         if (array[i]>max)
