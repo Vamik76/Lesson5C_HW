48e1ae06b5ab9de5273e7f9630374a646c46f065
Vamik76 <merts_v@mail.ru>
2022-12-09 18:43:32 +1000

    HW5C#_task1end

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+// Диапазон трехзначных чисел, ввод случайный.
+Random random = new Random();
+int[]arr=new int [6];
+for (int i=0; i<arr.Length;i++)
+    arr[i]=random.Next(100, 999);
+Console.WriteLine(string.Join(", ",arr));
+Console.WriteLine("Even Numbers "+EvenElement(arr));
+
+int EvenElement(int[]array)
+{
+    int count=0;
+    int b=0;
+    for (int i=0; i<array.Length;i++)
+        if (b==array[i]%2)
+            count++;
+    return count;
+}
